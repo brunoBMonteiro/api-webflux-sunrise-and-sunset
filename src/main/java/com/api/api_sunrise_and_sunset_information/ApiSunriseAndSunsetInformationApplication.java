c0b5d3d2f2e8f7056aac9bf360ed5bc6b7d68a5b
brunoBMonteiro <brunobilheri@hotmail.com>
2022-05-13 11:57:02 -0300

    starting api

---
+++ src/main/java/com/api/api_sunrise_and_sunset_information/ApiSunriseAndSunsetInformationApplication.java
@@ -0,0 +1,13 @@
+package com.api.api_sunrise_and_sunset_information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSunriseAndSunsetInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSunriseAndSunsetInformationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/api_sunrise_and_sunset_information/ApiSunriseAndSunsetInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.api_sunrise_and_sunset_information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSunriseAndSunsetInformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
